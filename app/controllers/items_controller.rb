be89c7a61eb61ddbabacef6f672f27fd92d878fc
kikuchimami <judy.19910618@gmail.com>
2023-08-28 15:03:28 +0900

    商品情報編集機能修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    #redirect_to root_path if current_user != @item.user||@item.purchase != nil
+    redirect_to root_path if current_user != @item.user
+    #||@item.order != nil
   end
 
   def update
